ce78f0a1f277c56589c35bd1a89344f6f52ffa15
Sabih Ismail <sabih.ismail@gmail.com>
2026-03-07 16:44:00 -0500

    Initial Commit

---
+++ ScreenShot/ScreenShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotApp.swift
+//  ScreenShot
+//
+//  Created by zeeN on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
